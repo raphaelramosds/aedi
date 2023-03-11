e3cbcd45899a90b824bd1618a6591b18b4eb0b39
raphaelramosds <raphael.opensource@gmail.com>
2023-03-11 16:28:01 -0300

    feat: more array list methods

--- arraylist/arraylist2.h
+++ arraylist/arraylist2.h
@@ -1,53 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct arraylist {
-    int* vetor;
+struct arraylist
+{
+    int *vetor;
     int qtdade;
     int capacidade;
 };
 
-struct arraylist* inicializar(int capacidade) {
-    struct arraylist* lista = (struct arraylist*)malloc(sizeof(struct arraylist));
-    lista->vetor = (int*)calloc(capacidade, sizeof(int));
+struct arraylist *inicializar(int capacidade)
+{
+    struct arraylist *lista = (struct arraylist *)malloc(sizeof(struct arraylist));
+    lista->vetor = (int *)calloc(capacidade, sizeof(int));
     lista->qtdade = 0;
     lista->capacidade = capacidade;
     return lista;
 }
 
-int obterElementoEmPosicao(struct arraylist* lista, int posicao) {
-    //TODO
+int obterElementoEmPosicao(struct arraylist *lista, int posicao)
+{
+    if (posicao < 0 || posicao > lista->capacidade - 1) {
+        printf("Posicao invalida: %d", posicao);
+        return 0;
+    }
+    return lista->vetor[posicao];
 }
 
-void duplicarCapacidade(struct arraylist* lista) {
-    //TODO
+void duplicarCapacidade(struct arraylist *lista)
+{
+    // TODO
 }
 
-void inserirElementoNoFim(struct arraylist* lista, int valor) {
-    //TODO
+void inserirElementoNoFim(struct arraylist *lista, int valor)
+{
+    lista->vetor[lista->qtdade] = valor;
+    lista->qtdade++;
 }
 
-void inserirElementoEmPosicao(struct arraylist* lista, int valor, int posicao) {
-    //TODO
+void inserirElementoEmPosicao(struct arraylist *lista, int valor, int posicao)
+{
+    // TODO
 }
 
-void atualizarElemento(struct arraylist* lista, int valor, int posicao) {
-    //TODO
+void atualizarElemento(struct arraylist *lista, int valor, int posicao)
+{
+    // TODO
 }
 
-void removerElementoNoFim(struct arraylist* lista) {
-    //TODO
+void removerElementoNoFim(struct arraylist *lista)
+{
+    lista->vetor[lista->qtdade - 1] = 0;
+    lista->qtdade--;
 }
 
-void removerElementoEmPosicao(struct arraylist* lista, int posicao) {
-    //TODO
+void removerElementoEmPosicao(struct arraylist *lista, int posicao)
+{
+    // TODO
 }
 
-void exibirLista(struct arraylist* lista) {
+void exibirLista(struct arraylist *lista)
+{
     printf("[");
-    for (int i = 0; i < lista->qtdade; i++) {
+    for (int i = 0; i < lista->qtdade; i++)
+    {
         printf("%d", lista->vetor[i]);
-        if (i < lista->qtdade - 1) {
+        if (i < lista->qtdade - 1)
+        {
             printf(", ");
         }
     }
--- arraylist/main.c
+++ arraylist/main.c
@@ -2,5 +2,29 @@
 
 int main()
 {
+    struct arraylist* l = inicializar(10);
+
+    inserirElementoNoFim(l,10);
+    inserirElementoNoFim(l,20);
+    inserirElementoNoFim(l,50);
+    inserirElementoNoFim(l,30);
+    inserirElementoNoFim(l,2);
+    inserirElementoNoFim(l,9);
+    exibirLista(l);
+
+    printf("\n");
+    printf("l[2] = %d", obterElementoEmPosicao(l,2));
+
+    printf("\n");
+    printf("l[0] = %d", obterElementoEmPosicao(l,0));
+
+    removerElementoNoFim(l);
+    removerElementoNoFim(l);
+    removerElementoNoFim(l);
+
+    printf("\n");    
+    exibirLista(l);
+
+
     return 0;
 }
