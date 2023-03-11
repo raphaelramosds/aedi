2989bfa56e88c125ff1d5c1d5eeab530a27bd6c3
raphaelramosds <raphael.opensource@gmail.com>
2023-03-11 14:59:51 -0300

    first commit

---
+++ linkedlists/main.c
@@ -0,0 +1,19 @@
+#include "linkedlist2.h"
+
+int main()
+{
+  struct linkedlist *ll = inicializar();
+
+  inserirElementoNoInicio(ll, 12);
+  inserirElementoNoInicio(ll, 14);
+  inserirElementoNoInicio(ll, 15);
+  inserirElementoNoFim(ll, 90);
+  inserirElementoNoFim(ll, 100);
+  inserirElementoNoInicio(ll, 20);
+  inserirElementoNoInicio(ll, 30);
+  imprimirLista(ll);
+  printf("\n");
+  removerElementoEmPosicao(ll, 0);
+  imprimirLista(ll);
+  printf("\n");
+}
