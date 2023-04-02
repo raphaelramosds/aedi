8e8532bad87856bec3a1ddd36bc39eb567fbc7d8
raphaelramosds <raphael.opensource@gmail.com>
2023-04-02 16:16:53 -0300

    feat: stacks implementation

--- stacks/helper/arraystack2.go
+++ stacks/helper/arraystack2.go
@@ -1,38 +1,66 @@
 package helper
 
+import "errors"
+
 // Basic Structure
 
 type ArrayStack struct {
 	values []int
-	tam    int
+	size   int
 }
 
 // Internal functions
 
 func (as *ArrayStack) double() {
-
+	doubledArray := make([]int, 2*len(as.values))
+	for i := 0; i < as.size; i++ {
+		doubledArray[i] = as.values[i]
+	}
+	as.values = doubledArray
 }
 
-func (as *ArrayStack) Init(size int) {
-
+func (as *ArrayStack) Init(size int) error {
+	if size > 0 {
+		as.size = 0
+		as.values = make([]int, size)
+		return nil
+	} else {
+		return errors.New("can't init an array with size <= 0")
+	}
 }
 
 // Interface functions
 
-func (as *ArrayStack) Push(value int) {}
+func (as *ArrayStack) Push(value int) {
+	if as.size == len(as.values) {
+		as.double()
+	}
+	as.values[as.size] = value
+	as.size++
+}
 
 func (as *ArrayStack) Pop() (int, error) {
-	return -1, nil
+	if as.size == 0 {
+		return -1, errors.New("stack is empty")
+	} else {
+		removedElement := as.values[as.size-1]
+		as.size--
+		return removedElement, nil
+	}
 }
 
 func (as *ArrayStack) Peek() (int, error) {
-	return -1, nil
+	if as.size == 0 {
+		return -1, errors.New("stack is empty")
+	} else {
+		return as.values[as.size-1], nil
+	}
 }
 
 func (as *ArrayStack) Empty() bool {
-	return as.tam == 0
+	return as.size == 0
 }
 
 func (as *ArrayStack) Size() int {
-	return as.tam
+	return as.size
 }
--- stacks/helper/linkedstack2.go
+++ stacks/helper/linkedstack2.go
@@ -1,5 +1,7 @@
 package helper
 
+import "errors"
+
 // Basic structures
 
 type Node struct {
@@ -9,27 +11,56 @@ type Node struct {
 
 type LinkedStack struct {
 	head *Node
-	tam  int
+	size int
 }
 
 // Public functions
 
 func (ls *LinkedStack) Push(value int) {
-
+	newNode := Node{value, nil}
+	curr := ls.head
+	if curr != nil {
+		for curr.next != nil {
+			curr = curr.next
+		}
+		curr.next = &newNode
+	} else {
+		ls.head = &newNode
+	}
+	ls.size++
 }
 
 func (ls *LinkedStack) Pop() (int, error) {
-	return -1, nil
+	if ls.size == 0 {
+		return -1, errors.New("stack is empty")
+	} else {
+		curr := ls.head
+		for i := 0; i < ls.size-1; i++ {
+			curr = curr.next
+		}
+		removedNode, _ := ls.Peek()
+		curr.next = nil
+		ls.size--
+		return removedNode, nil
+	}
 }
 
 func (ls *LinkedStack) Peek() (int, error) {
-	return -1, nil
+	if ls.size == 0 {
+		return -1, errors.New("stack is empty")
+	} else {
+		curr := ls.head
+		for curr.next != nil {
+			curr = curr.next
+		}
+		return curr.value, nil
+	}
 }
 
 func (ls *LinkedStack) Empty() bool {
-	return ls.tam == 0
+	return ls.size == 0
 }
 
 func (ls *LinkedStack) Size() int {
-	return ls.tam
+	return ls.size
 }
