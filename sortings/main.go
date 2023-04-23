05c5f0dcbd9b5cd59bb7bce68352a2d6d77dd826
raphaelramosds <raphael.opensource@gmail.com>
2023-04-23 18:17:12 -0300

    feat: bruh

--- sortings/main.go
+++ sortings/main.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	"dca0208.com/sortings/helper"
-)
-
 func main() {
-	s := helper.Init(10)
-	helper.Display(s[:])
-	helper.SelectionSort(s[:])
-	helper.Display(s[:])
+	// Silence is golden
 }
