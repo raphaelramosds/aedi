18f414c8ef5dc82f340747e8fa2e6a6f8819b989
raphaelramosds <raphael.opensource@gmail.com>
2023-05-27 08:58:06 -0300

    feat: size  challenge

--- bst/helper/challenges.go
+++ bst/helper/challenges.go
@@ -12,7 +12,7 @@ func (bstNode *BstNode) isBst() bool {
 		return true
 	}
 
-	// Análise do filhos
+	// Inspeção do filhos esquerdo e direito
 	if bstNode.left != nil && bstNode.value < bstNode.left.value {
 		return false
 	}
@@ -26,7 +26,22 @@ func (bstNode *BstNode) isBst() bool {
 }
 
 func (bstNode *BstNode) Size() int {
-	return -1
+	// Se visitei um nó, a contagem deve começar com 1
+	count := 1
+
+	// Inspeção dos filhos esquerdo e direito
+
+	if bstNode.left != nil {
+		// Acumule a quantidade nós da subárvore anterior
+		count += bstNode.left.Size()
+	}
+
+	if bstNode.right != nil {
+		count += bstNode.right.Size()
+	}
+
+	// Retorna quantidade de nós da subárvore
+	return count
 }
 
 func createBst(v []int) *BstNode {
--- bst/helper/challenges_test.go
+++ bst/helper/challenges_test.go
@@ -42,3 +42,33 @@ func TestIsBstWithABST(t *testing.T) {
 		t.Errorf("This binary tree is a BST")
 	}
 }
+
+func TestSizeWithRootNode(t *testing.T) {
+	root := &BstNode{value: 5}
+	result := root.Size()
+	expected := 1
+	if result != expected {
+		t.Errorf("This three has only one node")
+	}
+}
+
+func TestSize(t *testing.T) {
+	root := &BstNode{value: 5}
+
+	root.left = &BstNode{value: 2}
+	root.left.left = &BstNode{value: 1}
+	root.left.right = &BstNode{value: 4}
+
+	root.right = &BstNode{value: 7}
+	root.right.left = &BstNode{value: 1}
+	root.right.right = &BstNode{value: 8}
+	root.right.right.left = &BstNode{value: 3}
+	root.right.right.right = &BstNode{value: 10}
+
+	result := root.Size()
+	expected := 9
+
+	if result != expected {
+		t.Errorf("Expected %d nodes, but got %d", expected, result)
+	}
+}
